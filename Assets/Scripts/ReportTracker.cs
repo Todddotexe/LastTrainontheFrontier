ef53e900f5de7471cf236d2b3f351215339f1daf
Henry R <henry.richardson.gamedesign@gmail.com>
2022-06-06 16:58:50 +1000

    Added (Scripts)
    -

--- Assets/Scripts/ReportTracker.cs
+++ Assets/Scripts/ReportTracker.cs
@@ -10,7 +10,7 @@ public class ReportTracker : MonoBehaviour
 
     public GameObject reportSheet;
     public GameObject reportText;
-    public GameObject reportPersonal;
+    public Text reportPersonal;
 
     public GameObject reportLight;
 
@@ -28,7 +28,7 @@ public class ReportTracker : MonoBehaviour
         if (DayTracker.currentDay == 1)
         {
             reportText.GetComponent<Text>().text = "Departure successful. All passangers are accounted for and the cargo has been checked. VIP has settled into accomodations nicely.";
-            reportPersonal.GetComponent<Text>().text = "On a personal note Captain, our departure was somewhat late for the season. A small but palpable tension can be felt amongst the passangers. I will let you know if morale is severly impacted, but for now it would be best to keep the temperment of your passengers in mind.";
+            TextWriter.AddWriter_Static(reportPersonal, "On a personal note Captain, our departure was somewhat late for the season. A small but palpable tension can be felt amongst the passangers. I will let you know if morale is severly impacted, but for now it would be best to keep the temperment of your passengers in mind.", 0.03f, true);
             DayTracker.bedButton.SetActive(true);
         }
         if (DayTracker.currentDay == 2)
@@ -41,12 +41,12 @@ public class ReportTracker : MonoBehaviour
             if (IntercomTracker.Stowaway == true)
             {
                 reportText.GetComponent<Text>().text = "All passangers accounted for, cargo levels stable. VIP is comfortable, save for minor distasteful remarks. Stowaway situation unchanged, medical staff monitoring.";
-                reportPersonal.GetComponent<Text>().text = "As per my last report Captain, I'll continue to update you in an unofficial capacity. Morale appears to be on the rise, especially among the workers. The VIP doesn't seem too happy with the potential risk, but I've managed to calm him. All in all, as long as we have no upsets, it looks as though we've settled in for an easy journey.";
+                TextWriter.AddWriter_Static(reportPersonal, "As per my last report Captain, I'll continue to update you in an unofficial capacity. Morale appears to be on the rise, especially among the workers. The VIP doesn't seem too happy with the potential risk, but I've managed to calm him. All in all, as long as we have no upsets, it looks as though we've settled in for an easy journey.", 0.03f, true);
             }
             if (IntercomTracker.Stowaway == false)
             {
                 reportText.GetComponent<Text>().text = "All passangers accounted for, cargo levels stable. VIP is comfortable.";
-                reportPersonal.GetComponent<Text>().text = "As per my last report Captain, I'll continue to update you in an unofficial capacity. Morale is declined slightly, with a few clusters of passangers whispering about your decision to remove the Stowaway. While I understand the logic behind your decision, some are taking it as an act of an abuse of authority. I've done my best to calm them, but the VIP seems content with making snide remarks to other passangers that only aggrivate them furthur. I'll let you know if things get out of hand.";
+                TextWriter.AddWriter_Static(reportPersonal, "As per my last report Captain, I'll continue to update you in an unofficial capacity. Morale is declined slightly, with a few clusters of passangers whispering about your decision to remove the Stowaway. While I understand the logic behind your decision, some are taking it as an act of an abuse of authority. I've done my best to calm them, but the VIP seems content with making snide remarks to other passangers that only aggrivate them furthur. I'll let you know if things get out of hand." , 0.03f, true);
             }
             DayTracker.bedButton.SetActive(true);
         }
@@ -55,12 +55,12 @@ public class ReportTracker : MonoBehaviour
             if (IntercomTracker.Stowaway == true)
             {
                 reportText.GetComponent<Text>().text = "All passangers accounted for, cargo levels stable. VIP remains cautious. Stowaway has become agitated, medical staff monitoring, travel between passanger cars has been minimized.";
-                reportPersonal.GetComponent<Text>().text = "Captain, it appears there may be cause for concern afterall. While the general mood remains optomistic, a small feeling of unease has seeped into the passangers given the Stowaways development. Medical staff are unsure if its even any sort of sickness, or just the temperment of the Stowaway. I'll let you know as soon as it becomes clear, but for now I've limited the travel of the passangers between cars.";
+                TextWriter.AddWriter_Static(reportPersonal, "Captain, it appears there may be cause for concern afterall. While the general mood remains optomistic, a small feeling of unease has seeped into the passangers given the Stowaways development. Medical staff are unsure if its even any sort of sickness, or just the temperment of the Stowaway. I'll let you know as soon as it becomes clear, but for now I've limited the travel of the passangers between cars.", 0.03f, true);
             }
             if (IntercomTracker.Stowaway == false)
             {
                 reportText.GetComponent<Text>().text = "All passangers accounted for, cargo levels stable. VIP has expressed agitation at a handful of passangers, but is otherwise in good health.";
-                reportPersonal.GetComponent<Text>().text = "Captain, it appears some discourse has errupted within the populace. The VIP's remarks have elevated to lecturing, and the passangers whispers have elevated into loud discussion. Combined, they have created an environment of arguements. Several times this morning I have been called to seperate the fueding parties. While I normally wouldn't inform you of the petty squabbles of your passangers, I thought it would be best for you to be notified early, and to expect an ultimatum in the coming days.";
+                TextWriter.AddWriter_Static(reportPersonal, "Captain, it appears some discourse has errupted within the populace. The VIP's remarks have elevated to lecturing, and the passangers whispers have elevated into loud discussion. Combined, they have created an environment of arguements. Several times this morning I have been called to seperate the fueding parties. While I normally wouldn't inform you of the petty squabbles of your passangers, I thought it would be best for you to be notified early, and to expect an ultimatum in the coming days.", 0.03f, true);
             }
             DayTracker.bedButton.SetActive(true);
         }
@@ -76,12 +76,12 @@ public class ReportTracker : MonoBehaviour
                 if (IntercomTracker.Outbreak == true)
                 {
                     reportText.GetComponent<Text>().text = "All passangers accounted for (initially some were missing), cargo levels stable. VIP is healthy, but has taken to self isolation. Quarantine holding stable.";
-                    reportPersonal.GetComponent<Text>().text = "Captain, I fear the sickness may be out of hand, and Quarantine has come too late. Several passangers that had previously been cleared has now comeback positive, with several being found lurking in the supply car during the morning count. I blame myself for not treating this issue with more urgency. Medical staff have maintained that the sickness will pass, so I will do my duty and attempt to curb its spread.";
+                    TextWriter.AddWriter_Static(reportPersonal, "Captain, I fear the sickness may be out of hand, and Quarantine has come too late. Several passangers that had previously been cleared has now comeback positive, with several being found lurking in the supply car during the morning count. I blame myself for not treating this issue with more urgency. Medical staff have maintained that the sickness will pass, so I will do my duty and attempt to curb its spread.", 0.03f, true);
                 }
                 if (IntercomTracker.Outbreak == false)
                 {
                     reportText.GetComponent<Text>().text = "Healthy passangers accounted for, cargo levels above projected loss. VIP is comfortable. Last aflicted passangers have been ejected.";
-                    reportPersonal.GetComponent<Text>().text = "It is done. Overall, 13 souls were ejected from the train last night, including the Stowaway and several medical personnel. Though it was a hard choice, it is my duty to inform you that our employeers expected losses on this journey, and so any anxiety you may feel about the security of your job is unfounded. I know its not a comforting thing to hear, but you must persevere for the sake of your remaing passangers.";
+                    TextWriter.AddWriter_Static(reportPersonal, "It is done. Overall, 13 souls were ejected from the train last night, including the Stowaway and several medical personnel. Though it was a hard choice, it is my duty to inform you that our employeers expected losses on this journey, and so any anxiety you may feel about the security of your job is unfounded. I know its not a comforting thing to hear, but you must persevere for the sake of your remaing passangers.", 0.03f, true);
                 }
             }
             if (IntercomTracker.Stowaway == false)
@@ -89,12 +89,12 @@ public class ReportTracker : MonoBehaviour
                 if (IntercomTracker.Dispute == true)
                 {
                     reportText.GetComponent<Text>().text = "All passangers accounted for, cargo levels stable. VIP is in high spirits.";
-                    reportPersonal.GetComponent<Text>().text = "Captain, I'm concerned. We're yet to reach our first checkpoint, and already I feel a strong sense of division within the train. The VIP trots around, comfortable in your support of him, while the passangers hush their conversations whenever a member of staff walks by. I understand it is part of your duty to ensure the comfort of the VIP, but its reached the point of being at the expense of the passangers. Next time a decision is too be made, the passangers may just make it for you.";
+                    TextWriter.AddWriter_Static(reportPersonal, "Captain, I'm concerned. We're yet to reach our first checkpoint, and already I feel a strong sense of division within the train. The VIP trots around, comfortable in your support of him, while the passangers hush their conversations whenever a member of staff walks by. I understand it is part of your duty to ensure the comfort of the VIP, but its reached the point of being at the expense of the passangers. Next time a decision is too be made, the passangers may just make it for you.", 0.03f, true);
                 }
                 if (IntercomTracker.Dispute == false)
                 {
                     reportText.GetComponent<Text>().text = "All passangers accounted for, cargo levels stable. VIP has expressed agitation.";
-                    reportPersonal.GetComponent<Text>().text = "I know it must have been hard, going against your orders Captain. But the mood aboard has brightened considerably. While the VIP sits dejected and quiet, the other passangers rejoice quietly that they aren't subject to a tone deaf Captain. Undoubtedly this decision will come back to bite you eventually, but for now the train is at peace.";
+                    TextWriter.AddWriter_Static(reportPersonal, "I know it must have been hard, going against your orders Captain. But the mood aboard has brightened considerably. While the VIP sits dejected and quiet, the other passangers rejoice quietly that they aren't subject to a tone deaf Captain. Undoubtedly this decision will come back to bite you eventually, but for now the train is at peace.", 0.03f, true);
                 }
             }
             DayTracker.bedButton.SetActive(true);
